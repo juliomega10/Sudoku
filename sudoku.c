daa1a0bff2314e03ce85fc97501a5d93675a06e6
blastin <lisboa.jeff@gmail.com>
2017-09-28 21:04:50 -0300

    Alterado modelo de Grade. Refatorado algoritmo único para analisar possibilidades de valores. Algoritmo para verificarValorNaGrade feito.

--- sudoku.c
+++ sudoku.c
@@ -14,10 +14,10 @@
 //estruturas
 struct Grade {
 
-    int linha : 4;
-    int linhaLimite : 4;
-    int coluna : 4;
-    int colunaLimite : 4;
+    int linha;
+    int linhaLimite;
+    int coluna;
+    int colunaLimite;
 
 };
 
@@ -26,19 +26,24 @@ void verificaValorNaLinha(Sudoku *p_sudoku, int linha, unsigned char valoresPoss
 
 void verificarValorNaColuna(Sudoku *p_sudoku, int coluna, unsigned char valoresPossiveis[]);
 
-void verificarValorNaGrade(Sudoku *p_sudoku, const struct Grade *grade, unsigned char *valoresPossiveis);
+void verificarValorNaGrade(Sudoku *p_sudoku, const struct Grade *grade, unsigned char valoresPossiveis[]);
 
 void gerarValoresDaGrade(struct Grade *grade, int linha, int coluna);
+
+void resolverValoresPossiveis(Sudoku *p_sudoku, int coluna, unsigned char *valoresPossiveis, int linha);
 
 void resolverPorMetodoSingle(Sudoku *p_sudoku);
 
 // interface requerida
 void mostrarPossiveis(Sudoku *, int, int);
 
+
 //main
 int main(int argc, char *argv[]) {
 
     int i;
+
+    int linha, coluna;
 
     unsigned char valoresPossiveis[] = {
             ZERO,
@@ -46,11 +51,30 @@ int main(int argc, char *argv[]) {
             CONST_QUANTIDADE_UNS, CONST_SOMATORIA_UM_A_NOVE
     };
 
+    struct Grade grade;
+
     Sudoku sudoku = {.nrCelulas = 10};
 
     lerMatriz(&sudoku);
 
-    verificaValorNaLinha(&sudoku, 7, valoresPossiveis);
+    mostrarMatriz(&sudoku);
+
+    linha = 6;
+
+    coluna = 7;
+
+    verificaValorNaLinha(&sudoku, linha, valoresPossiveis);
+
+    printf("após verificar valor na linha %d .. \n", linha);
+    for (i = 1; i <= INDICE_VALOR_UNICO; i++) {
+        printf("%d,", valoresPossiveis[i]);
+    }
+
+    putchar('\n');
+
+    verificarValorNaColuna(&sudoku, coluna, valoresPossiveis);
+
+    printf("após verificar valor na coluna %d .. \n", coluna);
 
     for (i = 1; i <= INDICE_VALOR_UNICO; i++) {
         printf("%d,", valoresPossiveis[i]);
@@ -58,19 +82,17 @@ int main(int argc, char *argv[]) {
 
     putchar('\n');
 
-    verificarValorNaColuna(&sudoku, 9, valoresPossiveis);
+    gerarValoresDaGrade(&grade,linha,coluna);
+
+    verificarValorNaGrade(&sudoku, &grade, valoresPossiveis);
+
+    printf("após verificar na grade linha [%d,%d] | coluna  [%d,%d].. \n",  grade.linha,grade.linhaLimite,grade.coluna,grade.colunaLimite);
 
     for (i = 1; i <= INDICE_VALOR_UNICO; i++) {
         printf("%d,", valoresPossiveis[i]);
     }
 
     putchar('\n');
-
-    struct Grade grade;
-
-    gerarValoresDaGrade(&grade,2,5);
-
-    printf("linha:%d\nlinha limite:%d\ncoluna:%d\ncoluna limite:%d\n", grade.linha,grade.linhaLimite,grade.coluna,grade.colunaLimite);
 
     // LOOP
 
@@ -85,59 +107,23 @@ int main(int argc, char *argv[]) {
 
 
 // Implementações
-void verificaValorNaLinha(Sudoku *p_sudoku, int linha, unsigned char valoresPossiveis[]) {
 
-    int coluna;
+inline void resolverValoresPossiveis(Sudoku *p_sudoku, int coluna, unsigned char *valoresPossiveis, int linha) {
+
     int indice;
+
     int valorPossivel;
 
-    for (coluna = 1; coluna < p_sudoku->nrCelulas; coluna = coluna + 1) {
+    indice = p_sudoku->G[linha][coluna] - ZERO;
 
-        indice = p_sudoku->G[linha][coluna] - ZERO;
+    valorPossivel = valoresPossiveis[indice] - ZERO;
 
-        valorPossivel = valoresPossiveis[indice] - ZERO;
+    valoresPossiveis[INDICE_QUANTIDADE_VALORES_POSSIVEIS] -= valorPossivel;
 
-        valoresPossiveis[INDICE_QUANTIDADE_VALORES_POSSIVEIS] -= valorPossivel;
+    valoresPossiveis[INDICE_VALOR_UNICO] -= indice * valorPossivel;
 
-        valoresPossiveis[INDICE_VALOR_UNICO] -= indice * valorPossivel;
+    valoresPossiveis[indice] = ZERO;
 
-        valoresPossiveis[indice] = ZERO;
-
-    }
-
-}
-
-void verificarValorNaColuna(Sudoku *p_sudoku, int coluna, unsigned char valoresPossiveis[]) {
-
-    int linha;
-    int indice;
-    int valorPossivel;
-
-    for (linha = 1; linha < p_sudoku->nrCelulas; linha = linha + 1) {
-
-        indice = p_sudoku->G[linha][coluna] - ZERO;
-
-        valorPossivel = valoresPossiveis[indice] - ZERO;
-
-        valoresPossiveis[INDICE_QUANTIDADE_VALORES_POSSIVEIS] -= valorPossivel;
-
-        valoresPossiveis[INDICE_VALOR_UNICO] -= indice * valorPossivel;
-
-        valoresPossiveis[indice] = ZERO;
-
-    }
-
-}
-
-void verificarValorNaGrade(Sudoku *p_sudoku, const struct Grade *grade, unsigned char *valoresPossiveis) {
-
-    int linha, coluna;
-
-    for (linha = grade->linha; linha < grade->linhaLimite; linha = linha + 1) {
-        for (coluna = grade->coluna; coluna < grade->colunaLimite; coluna = coluna + 1) {
-            /*TODO: Verificar valor em grade */
-        }
-    }
 }
 
 void gerarValoresDaGrade(struct Grade *grade, int linha, int coluna) {
@@ -170,15 +156,47 @@ void gerarValoresDaGrade(struct Grade *grade, int linha, int coluna) {
 
  */
 
- linha  = (linha  - 1) / 3;
- coluna = (coluna - 1) / 3;
+    linha  = (linha  - 1) / 3;
+    coluna = (coluna - 1) / 3;
 
- grade->linha = 1 + 3 * linha;
- grade->linhaLimite = 3 * (linha + 1);
- grade->coluna = 1 + 3 * coluna;
- grade->colunaLimite = 3 * (coluna + 1);
+    grade->linha = 1 + 3 * linha;
+    grade->linhaLimite = 3 * (linha + 1);
+    grade->coluna = 1 + 3 * coluna;
+    grade->colunaLimite = 3 * (coluna + 1);
 
 }
+
+void verificaValorNaLinha(Sudoku *p_sudoku, int linha, unsigned char valoresPossiveis[]) {
+
+    int coluna;
+
+    for (coluna = 1; coluna < p_sudoku->nrCelulas; coluna = coluna + 1) {
+        resolverValoresPossiveis(p_sudoku, coluna, valoresPossiveis, linha);
+    }
+
+}
+
+void verificarValorNaColuna(Sudoku *p_sudoku, int coluna, unsigned char valoresPossiveis[]) {
+
+    int linha;
+
+    for (linha = 1; linha < p_sudoku->nrCelulas; linha = linha + 1) {
+        resolverValoresPossiveis(p_sudoku, coluna, valoresPossiveis, linha);
+    }
+
+}
+
+void verificarValorNaGrade(Sudoku *p_sudoku, const struct Grade *grade, unsigned char valoresPossiveis[]) {
+
+    int linha, coluna;
+
+    for (linha = grade->linha; linha < grade->linhaLimite; linha = linha + 1) {
+        for (coluna = grade->coluna; coluna < grade->colunaLimite; coluna = coluna + 1) {
+            resolverValoresPossiveis(p_sudoku, coluna, valoresPossiveis, linha);
+        }
+    }
+}
+
 
 void resolverPorMetodoSingle(Sudoku *p_sudoku) {
 
